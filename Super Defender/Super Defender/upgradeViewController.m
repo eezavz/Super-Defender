bbb7b468ad1b03da8c27c218c903664ad54d89b8
Webfox <patrick@bohner.nl>
2013-11-11 13:02:55 +0100

    ViewControllers aangemaakt en Views verplaatst uit MenuView.

---
+++ Super Defender/Super Defender/upgradeViewController.m
@@ -0,0 +1,38 @@
+//
+//  upgradeViewController.m
+//  Super Defender
+//
+//  Created by Patrick Böhner on 11/11/13.
+//  Copyright (c) 2013 RoFuPaNi. All rights reserved.
+//
+
+#import "upgradeViewController.h"
+
+@interface upgradeViewController ()
+
+@end
+
+@implementation upgradeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
